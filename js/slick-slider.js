d9f9881628b78c98310589c7f5fbe0eecf3ab134
Max <yudinmax4444@gmail.com>
2021-02-03 19:10:00 +0200

    finished exam

---
+++ js/slick-slider.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $('.third-container__slides').slick({
+        arrows: true,
+        dots: true,
+        slidesToShow: 3,
+        autoplay: true,
+        autoplaySpeed: 3000
+    });
+});
+
+$(document).ready(function(){
+    $('.first-container__block').slick({
+        arrows: true,
+        dots: true,
+        slidesToShow: 1,
+    });
+});
+
+
+       
